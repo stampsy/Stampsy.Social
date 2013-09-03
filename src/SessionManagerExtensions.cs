8e22ac9cbdc8428fc7117cbda377fde1f08eb262
Dan Abramov <dan.abramov@gmail.com>
2013-09-03 18:14:53 +0400

    Refactor reauthorization out of WithSession

--- src/SessionManagerExtensions.cs
+++ src/SessionManagerExtensions.cs
@@ -6,6 +6,22 @@ namespace Stampsy.Social
 {
     internal static class SessionManagerExtensions
     {
+        static async Task<bool> Reauthorize (ServiceManager manager, Session session)
+        {
+            var service = session.Service;
+            var account = session.Account;
+
+            try {
+                account = await service.ReauthorizeAsync (account);
+                session = new Session (service, account);
+
+                await manager.SwitchSessionAsync (session, true);
+                return true;
+            } catch {
+                return false;
+            }
+        }
+
         static async Task<T> WithSession<T> (ServiceManager manager, Func<Task<T>> call, string [] scope, LoginOptions options, bool allowReauthorizeOrLogout = true)
         {
             if (!SessionManager.NetworkMonitor.IsNetworkAvailable)
@@ -27,18 +43,7 @@ namespace Stampsy.Social
 
             bool tryReauthorize = (ex.Kind == ApiExceptionKind.Unauthorized);
             if (tryReauthorize) {
-                Session reauthorizedSession = null;
-
-                try {
-                    var service = session.Service;
-                    var badAccount = session.Account;
-                    var reauthorizedAccount = await service.ReauthorizeAsync (badAccount);
-
-                    reauthorizedSession = new Session (service, reauthorizedAccount);
-                } catch { }
-
-                if (reauthorizedSession != null) {
-                    await manager.SwitchSessionAsync (reauthorizedSession, true);
+                if (await Reauthorize (manager, session)) {
                     return await call ();
                 }
             }
